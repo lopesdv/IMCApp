18753d9c1e4fe355ba40bd5770d4fc0a15d1e8e1
lopesdv <marcelo.n.lopes@hotmail.com>
2021-10-12 11:57:13 +0100

    Logica Situação Peso

--- v1/imc.js
+++ v1/imc.js
@@ -2,12 +2,13 @@ const nome = "IMC"; // declara uma constante e atribui o valor "Armazém" (um 's
 
 class Cliente {
     #nascimento; // Apenas campos privados devem obrigatóriamente ser declaradas
-    constructor(nome, apelido, genero, altura, peso) { 
+    constructor(nome, apelido, genero, altura, peso, imc) { 
         this.nome = nome; 
         this.apelido = apelido; 
         this.genero = genero; 
         this.altura = altura;
         this.peso = peso;
+        this.imc = (peso/(altura*altura)).toFixed(1); //função onde é feito o calculo com a formula IMC = peso / (altura x altura)
 
     }
     toString() { 
@@ -27,7 +28,27 @@ class TipoDesporto {
     }
 }
 
-function FormulaIMC() {  //preciso de criar a função onde é feito o calculo com a formula IMC = peso / (altura x altura)
+
+
+function SituacaoPeso(imc) {  //Logica para a Situação de Peso do Cliente com base no IMC Calculado em cima 
+    let SituacaoPeso = ""
+
+    if(imc < 18.5){
+        SituacaoPeso = "Baixo Peso"
+
+    } else if (imc >= 18.5 && imc <= 24.9 ){
+        SituacaoPeso = "Peso normal"
+
+    } else if (imc >= 25 && imc <= 35 ){
+
+        SituacaoPeso = "Peso em Excesso"
+    } else if (imc >35){
+
+        SituacaoPeso = "Obesidade"
+    }
+
+    return SituacaoPeso
+
 
   
 }
